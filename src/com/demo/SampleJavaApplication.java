145841ae9c62f704324d51c79309b28d8a7cdce9
aditya-bstack <aditya.gu@browserstack.com>
2023-06-29 13:14:17 +0530

    initial commit

---
+++ src/com/demo/SampleJavaApplication.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class SampleJavaApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aditya-bstack-test");
+	}
+
+}
